f31dd74bce6b318b59e37687a3dab075d01ed10e
Hustler Tech Academy <hustlertech86@gmail.com>
2026-06-04 23:45:15 +0300

    Create Services.tsx

---
+++ src/Services.tsx
@@ -0,0 +1,15 @@
+export default function Services() {
+  return (
+    <div>
+      <h1>Our Services</h1>
+
+      <ul>
+        <li>AI Solutions</li>
+        <li>Cyber Security</li>
+        <li>Website Development</li>
+        <li>Digital Marketing</li>
+        <li>Branding & Design</li>
+      </ul>
+    </div>
+  );
+}
