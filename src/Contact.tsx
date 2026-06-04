df425180172f469c6d0c25300c0a8030117c2360
Hustler Tech Academy <hustlertech86@gmail.com>
2026-06-04 23:46:27 +0300

    Create Contact.tsx

---
+++ src/Contact.tsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+
+      <p>Email: hustlertech86@gmail.com</p>
+
+      <p>Phone: 0730 496 475</p>
+
+      <p>Instagram: @hustlertech.ai</p>
+    </div>
+  );
+}
