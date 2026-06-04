43195a8f5da5314a3743a974d6d589ce247c9fc5
Hustler Tech Academy <hustlertech86@gmail.com>
2026-06-04 23:47:19 +0300

    Create Portfolio.tsx

---
+++ src/Portfolio.tsx
@@ -0,0 +1,13 @@
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+
+      <ul>
+        <li>AI Form Coach</li>
+        <li>Cyber Security Toolkit</li>
+        <li>Hustler Tech AI Website</li>
+      </ul>
+    </div>
+  );
+}
